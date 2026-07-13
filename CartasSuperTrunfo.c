525a0158eafb94b042f05f62820ea7511aca6a88
Leonardo Patricio Campos <94012986+leopcampo@users.noreply.github.com>
2026-07-13 16:50:45 -0300

    Desafio Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,7 +14,10 @@ int main() {
     float area1, area2;
     float pib1, pib2;
     int turistico1, turistico2;
+    float densidade1, densidade2;
+    float pibPerCapta1, pibPerCapta2;
 
+    
     // Entrada dos dados
 
     printf("Digite o primeiro estado: ");
@@ -59,6 +62,13 @@ int main() {
     printf("Digite a segunda quantidade de pontos turísticos: ");
     scanf("%d", &turistico2);
 
+    densidade1 = populacao1 / area1;
+
+    densidade2 = populacao2 / area2;
+
+    pibPerCapta1 = pib1 / populacao1;
+    pibPerCapta2 = pib2 / populacao2;
+
     // Exibição dos dados
 
     printf("\n==============================\n");
@@ -71,6 +81,8 @@ int main() {
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Pontos Turísticos: %d\n", turistico1);
+    printf("Densidade Populacional: %.2f hab/km²", densidade1);
+    printf("PIB per Capita : %.2f reais", pibPerCapta1);
 
     printf("\n==============================\n");
     printf("Carta 2\n");
@@ -82,6 +94,8 @@ int main() {
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Pontos Turísticos: %d\n", turistico2);
+    printf("Densidade Populacional: %.2f hab/km²", densidade2);
+    printf("PIB per Capita : %.2f reais", pibPerCapta2);
 
     return 0;
 }
